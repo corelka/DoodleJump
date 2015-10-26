63ef4921c6edb779a87a587b84e25deed9a7bcc9
corelka <nikolai.korniichuk@gmail.com>
2015-10-26 12:47:22 +0200

    added_background

--- DoodleJump/main.cpp
+++ DoodleJump/main.cpp
@@ -15,10 +15,20 @@ using namespace video;
 using namespace io;
 using namespace gui;
 
+video::ITexture* txt(ITexture* txt, IVideoDriver* drv, int newX, int newZ)
+{
+	IImage* back_img = drv->createImageFromData(txt->getColorFormat(), txt->getSize(), txt->lock(), false);
+	IImage* back_img_trg = drv->createImage(ECF_A1R5G5B5, dimension2du(newX, newZ));
+	back_img->copyToScaling(back_img_trg);
+	ITexture* texture = drv->addTexture("text", back_img_trg);
+	texture->grab();
+	return texture;
+}
+
 int main(int argc, char** argv)
 {
 	IrrlichtDevice *device =
-		createDevice(EDT_SOFTWARE, dimension2du(640, 480), 16,
+		createDevice(EDT_SOFTWARE, dimension2du(800, 600), 16,
 		false, false, false, 0);
 
 	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
@@ -26,31 +36,35 @@ int main(int argc, char** argv)
 	IVideoDriver* driver = device->getVideoDriver();
 	ISceneManager* smgr = device->getSceneManager();
 	IGUIEnvironment* guienv = device->getGUIEnvironment();
+	//smgr->addSkyDomeSceneNode(driver->getTexture("../models_obj/background.png"));
+	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!", rect<int>(10, 10, 200, 22), true);//pattern for text!
+	//smgr->addSkyDomeSceneNode(driver->getTexture("../models_obj/background.png"), 100, 100, 1.0f, 1.0f);
 	
-	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
-		rect<int>(10, 10, 200, 22), true);//pattern for text!
-	
-	
-	IAnimatedMesh* mesh = smgr->getMesh("../models_obj/knight.obj");
-	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode(mesh);
-
-	if (node)
+	IAnimatedMesh* hero_mesh = smgr->getMesh("../models_obj/knight.obj");
+	IAnimatedMeshSceneNode* hero = smgr->addAnimatedMeshSceneNode(hero_mesh);
+	if (hero)
 	{
-		node->setMaterialFlag(EMF_LIGHTING, false);
-		node->setFrameLoop(0, 310);
-		node->setMaterialTexture(0, driver->getTexture("../models_obj/knight.tga"));
+		hero->setMaterialFlag(EMF_LIGHTING, false);
+		hero->setFrameLoop(0, 310);
+		hero->setMaterialTexture(0, driver->getTexture("../models_obj/knight.tga"));
+		hero->setPosition(vector3df(0, 0, 0));
+		hero->setRotation(vector3df(0, 90, 0));
 	}
-	node->setPosition(vector3df(0, 0, 0));
-	ICameraSceneNode *camera = smgr->addCameraSceneNode(0, vector3df(5, 1, 9), vector3df(0,1,9));
-	node->setRotation(vector3df(0, 90, 0));
-
+	
+	ICameraSceneNode *camera = smgr->addCameraSceneNode(0, vector3df(10, 1, 9), vector3df(0,1,9));
+	
+	ITexture *back_tex = driver->getTexture("../models_obj/background.png");
+	ITexture *calc_back = txt(back_tex, driver, driver->getScreenSize().Width, driver->getScreenSize().Height);
+	
+	
+	
 	while (device->run())
 	{
 		driver->beginScene(true, true, SColor(0, 200, 200, 200));
-
+		driver->draw2DImage(calc_back, position2d < s32 >(0, 0), rect < s32 >(0, 0, driver->getScreenSize().Width, driver->getScreenSize().Height), 0, SColor(255, 255, 255, 255), true);
 		smgr->drawAll();
 		guienv->drawAll();
-
+		//camera->setTarget(camera->getTarget() + vector3df(0, 0, 0));
 		driver->endScene();
 	}
 	device->drop();
